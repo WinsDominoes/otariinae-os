32093616f1583e69072d3de54ef895973e932486
AtiusAmy <torv.device027@passmail.net>
2026-01-17 06:50:48 +0700

    Huge refactor

--- files/scripts/droidcam.sh
+++ files/scripts/droidcam.sh
@@ -2,3 +2,8 @@
 
 wget -O /tmp/droidcam-client.x86_64.rpm https://droidcam.app/go/droidCam.client.setup.rpm
 dnf install -y /tmp/droidcam-client.x86_64.rpm
+
+dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-cdrtools.repo
+dnf config-manager setopt fedora-cdrtools.enabled=0
+dnf -y install --enablerepo=fedora-cdrtools \
+    cdrecord mkisofs cdda2wav
